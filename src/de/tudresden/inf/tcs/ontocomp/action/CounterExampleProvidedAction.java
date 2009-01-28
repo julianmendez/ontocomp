c766fe5bb25951dc9c4d0d81174889f16a96f7da
Baris Sertkaya <sertkaya.baris@gmail.com>
2009-01-28 13:20:44 +0000

    Added copyright statement

--- src/de/tudresden/inf/tcs/ontocomp/action/CounterExampleProvidedAction.java
+++ src/de/tudresden/inf/tcs/ontocomp/action/CounterExampleProvidedAction.java
@@ -10,6 +10,25 @@ import de.tudresden.inf.tcs.fcaapi.FCAImplication;
 import de.tudresden.inf.tcs.oclib.action.AbstractExpertAction;
 import de.tudresden.inf.tcs.oclib.IndividualContext;
 
+/* 
+ * OntoComP: a Protégé plugin for completing OWL ontologies.
+ * Copyright (C) 2009  Baris Sertkaya
+ * 
+ * This file is part of OntoComP.
+ * OntoComP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OntoComP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+	
 /**
  * The expert action fired when a counterexample is provided.
  * @author Baris Sertkaya
